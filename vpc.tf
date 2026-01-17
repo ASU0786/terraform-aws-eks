024d33d2328a15b1f9afa373c3f867ad9dee1f75
Ashutosh_Devops <57325073+ASU0786@users.noreply.github.com>
2026-01-17 10:38:44 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-subnet-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
